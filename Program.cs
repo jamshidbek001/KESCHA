3224382ba4ce74dd1f6ecd85f0bde26260a40499
Jamshidbek__001 <inomiddinovjamshidbek@gmail.com>
2022-10-18 13:56:33 -0700

    INFRA: Upload project

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+Console.Write("What is your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello {name}";
+Console.WriteLine(greeting);
+
+Console.Write("How old are you: ");
+string ageAsString = Console.ReadLine();
+Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageAsString);
+Console.WriteLine("Successfully converted your age");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
