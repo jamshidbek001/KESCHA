519e327e0f3d4951ec97055d285cf83535810528
Jamshidbek__001 <inomiddinovjamshidbek@gmail.com>
2022-10-18 16:58:05 -0700

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -5,11 +5,17 @@ string name = Console.ReadLine();
 string greeting = $"Hello {name}";
 Console.WriteLine(greeting);
 
+int nameKescha = 3;
 Console.Write("How old are you: ");
 string ageAsString = Console.ReadLine();
 Console.WriteLine("Converting...");
 int age = Convert.ToInt32(ageAsString);
-Console.WriteLine("Successfully converted your age");
+int sum = age - nameKescha;
+
+
+
+Console.WriteLine($"You are older than KESCHA: {sum > 0}");
+
 
 
 
